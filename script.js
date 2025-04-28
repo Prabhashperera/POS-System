9d07fbc47fcdc57469440fe6fb28f03f77ae3b74
Prabhashperera <prabashmperera01@gmail.com>
2025-04-28 22:25:01 +0530

    Item Form Complete

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-let customerBtn = document.getElementsByClassName("customer_Clicked")[0];
-let homeBtn = document.getElementsByClassName("home_Clicked")[0];
-let customerSection = document.getElementsByClassName("customer_Section")[0];
-let homePageSection = document.getElementsByClassName("homePage_Section")[0];
+let customerBtn = $(".customer_Clicked");
+let homeBtn = $(".home_Clicked");
+let itemBtn = $(".item_Clicked");
+let customerSection = $(".customer_Section");
+let homePageSection = $(".homePage_Section");
+let itemPageSection = $(".items_Section");
 
 
-customerBtn.addEventListener("click" , () => {
-    customerSection.style.display = "block";
-    homePageSection.style.display = "none";
-    console.log("Clicked");
+itemBtn.on("click" , () => {
+    homePageSection.css("display" , "none");
+    customerSection.css("display" , "none")
+    itemPageSection.css("display" , "block");
 })
 
-homeBtn.addEventListener("click" , () => {
-    customerSection.style.display = "none";
-    homePageSection.style.display = "block"
+customerBtn.on("click" , () => {
+    customerSection.css("display" , "block");
+    homePageSection.css("display" , "none");
+    itemPageSection.css("display" , "none")
 })
 
-//Save
+homeBtn.on("click" , () => {
+    itemPageSection.css("display" , "none")
+    customerSection.css("display" , "none");
+    homePageSection.css("display" , "block");
+})
+
+//Customer Save
 let customerSaveBtn = $(".customer_Save_Clicked");
 let customerTable = $(".customer_Table");
 
@@ -35,7 +44,35 @@ customerSaveBtn.on("click" , () => {
             <td>${customerNumber}</td>
             </tr>
     `
-    customerTable.append(data)
+    customerTable.append(data) 
     console.log(data);
 
 })
+
+// Item Save
+
+let itemSaveBtn = $(".item_Save_Clicked");
+let itemTable = $(".item_Table");
+
+itemSaveBtn.on("click" , () => {
+
+    $(".item_ID").val("I001");
+    let itemID = $(".item_ID").val();
+    let ItemName = $(".item_Name").val();
+    let ItemPrice = $(".item_Price").val();
+    let ItemQty = $(".item_Qty").val();
+
+    let data = `
+            <tr>
+            <th scope="row">${itemID}</th>
+            <td>${ItemName}</td>
+            <td>${ItemPrice}</td>
+            <td>${ItemQty}</td>
+            </tr>
+    `
+    itemTable.append(data) 
+    console.log(data);
+
+})
+
+
