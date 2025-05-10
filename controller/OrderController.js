8e5f1de7b01d21ed53acdfb1db1fd2f42e420a95
Prabhashperera <prabashmperera01@gmail.com>
2025-05-10 11:06:19 +0530

    Order Section v11

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -21,3 +21,29 @@
         // $(".customer_ID").prop("disabled", true); //Disable ID Field
     }
 });
+
+
+// Item FIND 
+
+//Table Row selection
+$('.item_Table').on('click', 'tr', function(){
+    $('.item_Table tr').removeClass('table-active'); // clear previous selection
+    $(this).addClass('table-active'); // highlight selected row
+
+    let selectedItemID = $(this).find('td:eq(0)').text().trim();
+    let selectedItemName = $(this).find('td:eq(1)').text().trim();
+    let selectedItemPrice = $(this).find('td:eq(2)').text().trim();
+    let selectedItemQty = $(this).find('td:eq(3)').text().trim(); 
+    console.log("Row selected");
+    console.log(selectedItemID);
+    console.log(selectedItemName);
+
+    //After Selecting Customer.. all the inputs takes the values
+    if(selectedItemID != null) {
+        $(".order_Item_ID").text(selectedItemID);
+        $(".order_Item_Name").val(selectedItemName);
+        $(".order_Item_Price").val(selectedItemPrice);
+        $(".order_Item_Qty").val(selectedItemQty);
+        // $(".item_ID").prop("disabled", true); //Disable ID Field
+    }
+});
